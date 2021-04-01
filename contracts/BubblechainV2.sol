22061eadd70d984d8679024f5117c52688b085f6
Jehan <jehandacamara@hotmail.com>
2021-04-01 11:52:21 +0200

    Added timing constraint for merkle tree Bubblechain version

--- contracts/BubblechainV2.sol
+++ contracts/BubblechainV2.sol
@@ -4,35 +4,42 @@ contract BubblechainV2 {
 
   struct Root{
     bytes32 root;
+    uint256 expDate;
   //Add timing constraint
   }
 
   mapping(address => Root) roots;
 
-  function addVerificationKey(bytes32 root) public {
-    roots[msg.sender] = Root(root);
+  function addVerificationKey(bytes32 root, uint256 timeAdded) public {
+    uint256 timeStamp = block.timestamp + 900 + timeAdded;
+    roots[msg.sender] = Root(root, timeStamp);
 
   }
 
   function verifyMembership(address bubbleID,bytes32 leaf,bytes32[] memory proof) public view returns (bool){
     Root memory value = roots[bubbleID];
     bytes32 root = value.root;
-    bytes32 computedHash = leaf;
+    if (value.expDate >= now + 900){
+      bytes32 computedHash = leaf;
 
-    for (uint256 i = 0; i < proof.length; i++) {
-      bytes32 proofElement = proof[i];
+      for (uint256 i = 0; i < proof.length; i++) {
+        bytes32 proofElement = proof[i];
 
-      if (computedHash < proofElement) {
-        // Hash(current computed hash + current element of the proof)
-        computedHash = keccak256(abi.encodePacked(computedHash, proofElement));
-      } else {
-        // Hash(current element of the proof + current computed hash)
-        computedHash = keccak256(abi.encodePacked(proofElement, computedHash));
+        if (computedHash < proofElement) {
+          // Hash(current computed hash + current element of the proof)
+          computedHash = keccak256(abi.encodePacked(computedHash, proofElement));
+        } else {
+          // Hash(current element of the proof + current computed hash)
+          computedHash = keccak256(abi.encodePacked(proofElement, computedHash));
+        }
       }
+
+      // Check if the computed hash (root) is equal to the provided root
+      return computedHash == root;
     }
 
-    // Check if the computed hash (root) is equal to the provided root
-    return computedHash == root;
+    return false;
+
   }
 
 }
