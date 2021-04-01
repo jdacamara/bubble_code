27ea09cbaf6c854b3961cf4d1674e37035cfbf91
Jehan <jehandacamara@hotmail.com>
2021-04-01 11:10:58 +0200

    Timing constraint for the basic Bubblechain

--- contracts/Bubblechain.sol
+++ contracts/Bubblechain.sol
@@ -4,18 +4,29 @@ contract Bubblechain {
 
   struct VerificationKey{
     string verificationKey;
+    uint256 expDate;
   //Add timing constraint
   }
 
   mapping(address => VerificationKey) keys;
 
-  function addVerificationKey(string memory key_value) public {
-    keys[msg.sender] = VerificationKey(key_value);
+  function addVerificationKey(string memory key_value, uint256 timeAdded) public {
+    uint256 timeStamp = block.timestamp + 900 + timeAdded;
+    //timeStamp = now +1
+    keys[msg.sender] = VerificationKey(key_value, timeStamp);
 
   }
 
-  function retrieveVerificationKey(address identifier) view public returns (string memory){
-    return(keys[identifier].verificationKey);
+  function retrieveVerificationKey(address identifier) view public returns (string memory, uint256){
+    VerificationKey memory v = keys[identifier];
+    //return v
+    if(v.expDate <= now){
+      string memory a = v.verificationKey;
+      uint256 b = v.expDate;
+      return (a,b);
+    }else{
+      return ("0",0);
+    }
 
   }
 
