45b520a1e629290a00170b9ab51a77301e084c25
Jehan <jehandacamara@hotmail.com>
2021-05-15 19:52:58 +0200

    Update migrations to only add the latest version of Bubblechain

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,13 @@
 const Bubblechain = artifacts.require('Bubblechain')
 const BubblechainV2 = artifacts.require('BubblechainV2')
 const MerkleProof = artifacts.require('MerkleProof')
+const BubblechainV3 = artifacts.require('BubblechainV3')
+const BubblechainV4 = artifacts.require('BubblechainV4')
 
 module.exports = function (deployer) {
-  deployer.deploy(Bubblechain)
+  /*deployer.deploy(Bubblechain)
   deployer.deploy(BubblechainV2)
   deployer.deploy(MerkleProof)
+  deployer.deploy(BubblechainV3)*/
+  deployer.deploy(BubblechainV4)
 }
