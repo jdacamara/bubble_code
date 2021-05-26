ed9d65baeeac0e0c0059ac5c9be6f4d29be3a559
Jehan <jehandacamara@hotmail.com>
2021-05-26 20:58:40 +0200

    commented old migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
-const Bubblechain = artifacts.require('Bubblechain')
-const BubblechainV2 = artifacts.require('BubblechainV2')
-const MerkleProof = artifacts.require('MerkleProof')
-const BubblechainV3 = artifacts.require('BubblechainV3')
+//const Bubblechain = artifacts.require('Bubblechain')
+//const BubblechainV2 = artifacts.require('BubblechainV2')
+//const MerkleProof = artifacts.require('MerkleProof')
+//const BubblechainV3 = artifacts.require('BubblechainV3')
 const BubblechainV4 = artifacts.require('BubblechainV4')
 
 module.exports = function (deployer) {
